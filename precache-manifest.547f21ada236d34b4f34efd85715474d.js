94b0383c3f8913a034e2c7c82d68cb673d9544c2
ljdduncan <ljdduncan@gmail.com>
2020-07-07 22:22:53 -0400

    Updates

---
+++ precache-manifest.547f21ada236d34b4f34efd85715474d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87868651ecc7319afa0f03f1af4ce45d",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "0e739f3f6ea2e2368ce2",
+    "url": "/tictactoe/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c35602a6eb87114a9588",
+    "url": "/tictactoe/static/js/2.827857ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.827857ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e739f3f6ea2e2368ce2",
+    "url": "/tictactoe/static/js/main.3f2dee3e.chunk.js"
+  },
+  {
+    "revision": "8fd1009f88de2a8f7d1e",
+    "url": "/tictactoe/static/js/runtime-main.57baa19c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tictactoe/static/media/logo.5d5d9eef.svg"
+  }
+]);
